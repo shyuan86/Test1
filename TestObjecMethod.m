68d410473a4311bb0bf166717957954264239f39
chenshiyuan <shiyuanchen676@dingtalk.com>
2023-08-28 11:04:43 +0800

    更新打印方法

--- TestObjecMethod.m
+++ TestObjecMethod.m
@@ -1,1 +1,23 @@
-NSLog(@"Object C Test!");
+#import <Foundation/Foundation.h>
+#import <UIkit/UIkit.h>
+
+@interface TestObjecMethod
+
+-(void)printMethod;
+
+@end
+
+
+@implementation TestObjecMethod
+
+
+-(void)printMethod{
+    NSString* str = [[NSString alloc] initWithFormat:@"age is:%d",18];
+    NSLog(@"My %@");
+}
+
+@end
+
+
+
+
